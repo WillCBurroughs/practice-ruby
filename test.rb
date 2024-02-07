365e5ed371bfa674f62d09c995100128e213f93c
William Burroughs <willcburroughs@gmail.com>
2024-02-07 11:01:35 -0500

    Adding data types

--- test.rb
+++ test.rb
@@ -20,3 +20,14 @@ character_age = 23
 
 # Following is how to convert number to string 
 puts ("There was a man named " + character_name + " He was " + character_age.to_s + " years old.")
+
+
+# Data types 
+string_val = "Hello"
+integer_val = 12
+floating_point = 3.1
+
+boolean_val = true 
+nil_value = nil 
+undefined_value = undef
+
