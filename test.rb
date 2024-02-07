3a824798b1930e4324a87f929d10816e731e2962
William Burroughs <willcburroughs@gmail.com>
2024-02-07 10:55:50 -0500

    Saved changing type

--- test.rb
+++ test.rb
@@ -9,3 +9,14 @@ puts "/___|"
 puts "This is a new line to be added"
 
 puts "Testing second line addition"
+
+name = "Joshua"
+
+puts name
+
+
+character_name = "Will"
+character_age = 23
+
+# Following is how to convert number to string 
+puts ("There was a man named " + character_name + " He was " + character_age.to_s + " years old.")
